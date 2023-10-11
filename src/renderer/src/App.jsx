13026ea22d3de7d100071714e030425249852b47
Kiran kuyate <84271800+kirankuyate2157@users.noreply.github.com>
2023-10-11 14:10:50 +0530

    AUth added

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <>
       <div className='App w-full'>
+  
         <MailDashboard mails={data} />
       </div>
     </>
--- src/renderer/src/components/MailDashboard.jsx
+++ src/renderer/src/components/MailDashboard.jsx
@@ -1,4 +1,10 @@
-import React, { useState } from "react";
+// D:\Works\WNS\adv-mail-app\src\renderer\src\components\MailDashboard.jsx
+
+import React, { useState, useEffect } from 'react';
+import { getUserData, GoogleAuth, login } from './../services/firebaseConfig.js';
+import Login from './auth/Login';
+
+
 import MailList from "./MailList";
 import MailDetails from "./MailDetails";
 
@@ -11,6 +17,14 @@ const MailDashboard = ({ mails }) => {
   const [activeTab, setActiveTab] = useState("inbox");
   const [searchQuery, setSearchQuery] = useState("");
   const [isTabsExpanded, setTabsExpanded] = useState(true);
+
+
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+  });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userName, setUserName] = useState('');
 
   const filteredMails = mails
     .filter((mail) => mail.folder === activeTab)
@@ -52,6 +66,80 @@ const MailDashboard = ({ mails }) => {
     );
   };
 
+
+
+
+  useEffect(() => {
+    // Check the user's authentication status when the component loads
+    const checkAuthStatus = async () => {
+      const user = getUserData();
+      if (user) {
+        // User is authenticated
+        setIsAuthenticated(true);
+        setUserName(user.displayName || user.email);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
+  const handleChange = (e) =>
+    setUserData((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    if (!userData.email) {
+      alert('Email is required!');
+    }
+    if (!userData.password) {
+      alert('Password is required!');
+    }
+
+    try {
+      const status = await login(userData.email, userData.password);
+      if (status) {
+        alert('Login successful!');
+        const user = getUserData();
+        setIsAuthenticated(true);
+        setUserName(user.displayName || user.email);
+
+        // Store user data in local storage
+        localStorage.setItem('userData', JSON.stringify(user));
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const googleLogin = async () => {
+    try {
+      await GoogleAuth();
+      alert('Login successful with Google!');
+      const user = getUserData();
+      setIsAuthenticated(true);
+      setUserName(user.displayName || user.email);
+
+      // Store user data in local storage
+      localStorage.setItem('userData', JSON.stringify(user));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  // // Check for user data in local storage when the component loads
+  // useEffect(() => {
+  //   const storedUserData = localStorage.getItem('userData');
+  //   if (storedUserData) {
+  //     const userData = JSON.parse(storedUserData);
+  //     setIsAuthenticated(true);
+  //     setUserName(userData.displayName || userData.email);
+  //   } else {
+  //     // If no user data found, perform Google login
+  //     googleLogin();
+  //   }
+  // }, []);
+
   return (
     <div className='w-screen font-mono bg-slate-300 flex h-screen'>
       <div
@@ -91,8 +179,10 @@ const MailDashboard = ({ mails }) => {
         </button>
       </div>
       <div className='w-full  h-screen border-l border-gray-300'>
-        <div className='flex justify-start p-3  text-black pl-5  items-center bg-slate-50 gap-20'>
-          <h1 className='text-xl font-semibold '>Mail Dashboard</h1>
+        <div className='flex justify-between   p-3 pr-10 text-black pl-5  items-center bg-slate-50 '>
+          
+          <div className='flex justify-start gap-20'>
+            <h1 className='text-xl font-semibold '>Mail Dashboard</h1>
           <div className='px-2'>
             <input
               type='text'
@@ -100,6 +190,12 @@ const MailDashboard = ({ mails }) => {
               className='w-[30vw] p-1 px-3 bg-gray-200 text-gray-900 text-sm rounded-xl border'
               onChange={(e) => setSearchQuery(e.target.value)}
             />
+          </div>
+          </div>
+          <div>
+
+          {isAuthenticated?(<button  className=" p-1 px-3 bg-gray-200 text-gray-900 text-sm rounded-xl border" onClick={()=>googleLogin()}>{userName}</button>):(<button  className=" p-1 px-3 bg-gray-200 text-gray-900 text-sm rounded-xl border" onClick={()=>googleLogin()}>login</button>)}
+        
           </div>
         </div>
         {/* Mail List */}
